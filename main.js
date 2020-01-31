1281437bb000502361ac1ff81cb4d4ff7885854a
Quinn Elder <qre.turing@gmail.com>
2020-01-30 17:24:32 -0700

    Add day1

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-var closeIcon = document.getElementById("close-icon");
+// var closeIcon = document.getElementById("close-icon");
+// var welcomeUser = document.getElementById('welcome-header')
+
+//find the close-icon
+//click the close-icon
+//make removeBanner disapear
 var welcomeUser = document.getElementById('welcome-header')
+var closeIcon = document.getElementById("close-icon")
+console.log(closeIcon)
+closeIcon.addEventListener("click", removeBanner)
+function removeBanner(){
+  console.log("im in");
+  welcomeUser.style.display = "none";
+}
