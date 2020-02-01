d2e22e6fb748361acaa2f6a46b9bad7f862749fc
Quinn Elder <qre.turing@gmail.com>
2020-02-01 14:19:32 -0700

    Add border swap

--- main.js
+++ main.js
@@ -3,9 +3,23 @@
 //make removeBanner disapear
 var welcomeUser = document.getElementById('welcome-header')
 var closeIcon = document.getElementById("close-icon")
-console.log(closeIcon)
+
 closeIcon.addEventListener("click", removeBanner)
 function removeBanner(){
   console.log("im in");
   welcomeUser.style.display = "none";
 }
+
+
+
+
+var clipboard = document.querySelector('.img-clipboard')
+var clickWallet = document.querySelector(".wallet-img")
+clickWallet.addEventListener("click", moveBlueBorder)
+function moveBlueBorder() {
+  clipboard.classList.remove("blue-border");
+  clickWallet.classList.add("blue-border");
+}
+// Creating an addEventListener for click on wallet to add border
+// on click run function to add border to Wallet
+// once wallet clicked inner.html runs
