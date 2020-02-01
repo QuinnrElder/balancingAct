58cf12cb36775722d422a4f39d3ed92437cb14e9
Quinn Elder <qre.turing@gmail.com>
2020-02-01 15:09:12 -0700

    Add Saturdays work

--- main.js
+++ main.js
@@ -13,12 +13,30 @@ function removeBanner(){
 
 
 
-var clipboard = document.querySelector('.img-clipboard')
+var clickClipboard = document.querySelector('.img-clipboard')
 var clickWallet = document.querySelector(".wallet-img")
-clickWallet.addEventListener("click", moveBlueBorder)
-function moveBlueBorder() {
-  clipboard.classList.remove("blue-border");
+var clickWoman = document.querySelector(".img-woman")
+
+clickClipboard.addEventListener("click", clipboardBorder)
+clickWallet.addEventListener("click", walletBorder)
+clickWoman.addEventListener("click", womanBorder)
+// add blue-border to wallet
+function walletBorder() {
+  clickClipboard.classList.remove("blue-border");
   clickWallet.classList.add("blue-border");
+  clickWoman.classList.remove("blue-border")
+}
+// add blue border to woman-img
+function womanBorder() {
+  clickClipboard.classList.remove("blue-border");
+  clickWallet.classList.remove("blue-border");
+  clickWoman.classList.add("blue-border")
+}
+// add blue border to clipboard
+function clipboardBorder() {
+  clickClipboard.classList.add("blue-border");
+  clickWallet.classList.remove("blue-border");
+  clickWoman.classList.remove("blue-border")
 }
 // Creating an addEventListener for click on wallet to add border
 // on click run function to add border to Wallet
